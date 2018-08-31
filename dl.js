1bb5f20207abd4e5ebb17e93a0785409c17b1dc2
Naoki KODAMA <naoki.koda@gmail.com>
2018-08-31 16:22:08 +0900

    improve play.js and add package.json

--- dl.js
+++ dl.js
@@ -1,14 +1,58 @@
 var fs = require('fs');
 var youtubedl = require('youtube-dl');
-var video = youtubedl('https://www.youtube.com/watch?v=nur5Kr9rfyA',
-  // Optional arguments passed to youtube-dl.
-  ['--format=18'],
-  // Additional options can be given for calling `child_process.execFile()`.
+
+var youtube_uri = null;
+var file_name = 'video.mp4';
+var quality = 137; // 1080p
+
+if (process.argv.length < 3){
+  console.log("node playtube.js youtube_uri");
+  process.exit(1);
+}else{
+  youtube_uri = process.argv[2];
+}
+
+if (process.argv.length >= 4){
+  file_name = process.argv[3];
+}
+
+if (process.argv.length >= 5){
+  quality = process.argv[4];
+}
+
+console.log("uri: " + youtube_uri);
+console.log("file_name: " + file_name);
+console.log("quality: " + quality);
+
+var video = youtubedl(youtube_uri,
+  ['--format=' + quality],
   { cwd: __dirname });
-// Will be called when the download starts.
+
+console.log('Getting information..');
+
+var size = 0;
 video.on('info', function(info) {
   console.log('Download started');
-  console.log('filename: ' + info.filename);
+  console.log('filename: ' + info._filename);
   console.log('size: ' + info.size);
+
+  size = info.size;
+  video.pipe(fs.createWriteStream(file_name));
 });
-video.pipe(fs.createWriteStream('myvideo.mp4'));
+
+
+var pos = 0;
+video.on('data', function data(chunk) {
+  pos += chunk.length;
+  if (size) {
+    var percent = (pos / size * 100).toFixed(2);
+    process.stdout.cursorTo(0);
+    process.stdout.clearLine(1);
+    process.stdout.write(percent + '%');
+  }
+});
+
+video.on('end', function(){
+  console.log('');
+  console.log('Download end');
+});
--- play.js
+++ play.js
@@ -1,3 +1,31 @@
 var Omx = require('node-omxplayer');
 
-var player = Omx(process.argv[2]);
+console.log(process.argv.length);
+
+var files = shuffle(process.argv.slice(2));
+console.log(files);
+
+function play_head_in_files(){
+  if (files.length == 0) return;
+
+  var file = files.shift();
+  files.push(file);
+  var player = Omx(file);
+  player.on('close', function(){
+    play_head_in_files();
+  });
+}
+
+function shuffle(array){
+  for(var i = array.length - 1; i > 0; i--){
+    var r = Math.floor(Math.random() * (i + 1));
+    var tmp = array[i];
+    array[i] = array[r];
+    array[r] = tmp;
+  }
+
+  return array;
+}
+
+play_head_in_files();
+
